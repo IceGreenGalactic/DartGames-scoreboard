65062cd489c9b09b4db9b2e0e63eed32fea39e2c
Kennel-Editor <125923103+IceGreenGalactic@users.noreply.github.com>
2025-10-29 23:07:20 +0100

    implements 301 rules

--- src/components/rules/index.js
+++ src/components/rules/index.js
@@ -1,9 +1,11 @@
 import { GameRules501 } from "./GameRules501";
+import { GameRules301 } from "./GameRules301";
 import { GameRulesClock } from "./GameRulesClock";
 import { GameRulesKiller } from "./GameRulesKiller";
 
 export const rulesByGame = {
   501: GameRules501,
+  301: GameRules301,
   clock: GameRulesClock,
   killer: GameRulesKiller,
 };
